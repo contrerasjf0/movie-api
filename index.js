bb6585a5f5de3e6a9943984392beff435e8ff8ab
Juan Francisco <contrerasjf.0@hotmail.com>
2020-07-08 19:13:20 -0500

    :sparkles: feature(errors): Implement Boom library to handle the error in the middlewares
    * The Boom library was installed.
    * The Boom library was implemented in the erroHandlers file.
    * The wrapErrors middleware was implemented to convert the non-boom errors to Boom errors.
    * The notFoundHandler middleware was implemented to handle the responses for resources not found.
    * The wrapErrors and notFoundHandler methods were implemented in index file.
    
    Resolve: C-33

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const app = express();
 
 const {  config } = require('./config/index');
 const moviesApi = require('./routes/movies');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 const {
   logErrors,
+  wrapErrors,
   errorHandler
 } = require('./utils/middleware/errorHandlers');
 
 // body parser
 app.use(express.json());
 
+// routes
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function(){
